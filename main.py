7604605ac3b605f9a1c2a133b4abaa6b12b9bae8
Tyler Tharp <tharpta@gmail.com>
2023-08-10 13:37:16 -0700

    docstrings

--- main.py
+++ main.py
@@ -10,6 +10,25 @@ linked_lists=[]
 all_algorithms = sorting_algos + graph_algos + linked_lists
 
 def algo_selector(remaining_algorithms):
+    """
+    algo_selector function
+
+    Parameters:
+    - remaining_algorithms (list): The list of algorithm names that have not yet been studied. 
+
+    Functionality:
+    - Prints a message and restarts the list when all algorithms have been studied.
+    - Randomly selects an algorithm from the remaining list to print.
+    - Removes the selected algorithm from the remaining list.
+    - Prompts the user to continue or quit.
+    - Breaks the loop if user enters 'n' to quit.
+    - Continues looping and selecting algorithms until user quits.
+
+    The all_algorithms list contains all algorithm names.
+    The algo_selector function takes the full list as a parameter initially.
+    It removes algorithms as they are selected to print.
+    When the remaining list is empty, it resets it back to the full list.
+    """
     while True:
         if len(remaining_algorithms) == 0:
             print("All algorithms have been studied! Starting list over")
